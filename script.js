502fc6ba1e4fd745ff49b76a5b9b323c8c32a0f9
Kyle Mosier <mosierkyle@gmail.com>
2023-08-14 20:55:46 -0700

    Create function that makes board
    Created a function that makes the board based off of an inputed number of sqaures. The default number of squares is 16.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+const board = document.querySelector('#board');
+document.createElement('div')
+
+function makeBoard(num = 16) {
+    let square = document.createElement('div')
+    for(let i = 0; i < num; i++) {
+        board.appendChild(document.createElement('div'));
+    } 
+   
+    let squares = board.children
+    for(let i = 0; i < squares.length; i++){
+        squares[i].className = 'pixel';
+        squares[i].style.height = `${((Math.sqrt(num)/num) * 600)}px` ;
+        squares[i].style.width = `${((Math.sqrt(num)/num) * 600)}px` ;
+    }
+}
+
+makeBoard(75);
+
+
